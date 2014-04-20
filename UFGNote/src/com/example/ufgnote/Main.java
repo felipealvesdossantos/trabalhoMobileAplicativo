b3b0d2f1a780b6eae8f0947dff36d80edf47fac5
Felipe <felipealvesdossantosdba@gmail.com>
2014-04-20 15:05:57 -0300

    Primeiro Commit da disciplina de: Mobile

---
+++ UFGNote/src/com/example/ufgnote/Main.java
@@ -0,0 +1,87 @@
+package com.example.ufgnote;
+
+//import br.ufg.inf.fs.android.a01.R;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Main extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void onClickMenuConfigurar(MenuItem item) {
+	        Toast toast = Toast.makeText(this, "Configurar...", Toast.LENGTH_LONG);
+	        toast.show();
+	}
+	
+	public void onClickMenuSair(MenuItem item) {
+        finish();
+    }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		 switch (item.getItemId()) {
+         case R.id.menu_configurar:
+             onClickMenuConfigurar(item);
+             return true;
+
+         case R.id.menu_sair:
+             onClickMenuSair(item);
+             return true;
+
+         default:
+             return super.onOptionsItemSelected(item);
+     }
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
